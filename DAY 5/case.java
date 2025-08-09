35be3200e45d7522f709d81c8b92025408b71305
Shanmuganathan <samu81768@gmail.com>
2025-08-09 17:19:26 +0530

    switch case

---
+++ DAY 5/case.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.util.*;
+class grade
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter tamil mark:");
+int t=sc.nextInt();
+switch(t)
+{
+case 1:
+System.out.println("grade A");
+break;
+case 2:
+System.out.println("grade b");
+break;
+case 3:
+System.out.println("grade C");
+break;
+}
+}
+}
