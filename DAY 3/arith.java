067bfddbfb00286785c5c8215cd1eacef6a04147
Shanmuganathan <samu81768@gmail.com>
2025-08-09 17:15:24 +0530

    operator

---
+++ DAY 3/arith.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.*;
+class arith
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter value of a");
+int a=sc.nextInt();
+System.out.println("enter value of b");
+int b=sc.nextInt();
+double c=a/b;
+System.out.println("enter value of add"+(a+b));
+System.out.println("enter value of b"+(a-b));
+System.out.println("enter value of b"+a*b);
+System.out.println("enter value of b"+c);
+
+}
+}
---
+++ DAY 3/vote.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+import java.util.*;
+class voterid
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter age:");
+		double a=sc.nextInt();
+		
+			if (a>=18){
+		System.out.println("your are eligible");}
+		
+			else{
+				System.out.println("your not eligible");
+		}
+		
+	}
+	
+
+}
